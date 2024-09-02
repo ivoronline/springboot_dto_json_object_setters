6a273981bf2ba39f0f8a65a69b8e3d6c5620bc64
ivoronline <ivoronline@gmail.com>
2024-09-02 12:19:48 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_dto_json_object_setters/DTO/PersonDTO.java
+++ src/main/java/com/ivoronline/springboot_dto_json_object_setters/DTO/PersonDTO.java
@@ -3,11 +3,19 @@ package com.ivoronline.springboot_dto_json_object_setters.DTO;
 public class PersonDTO {
 
   //PROPERTIES
-  public String  propName;
+  public String  propName = "unknown";
   public Integer propAge;
 
   //SETTERS
-  private void setName(String  reqName) { this.propName = reqName; }
-  private void setAge (Integer reqAge ) { this.propAge  = reqAge;  }
-
+  public void setName(String  reqName) {
+    System.out.println("setName()");
+    propName = reqName;
+  }
+  
+  public void setAge (Integer reqAge ) {
+    System.out.println("setAge()" );
+    propAge = reqAge;
+    if(propAge == null) { propAge = 0; }
+  }
+  
 }
